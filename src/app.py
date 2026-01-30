87502824b63e600ed733f3c6b7b3156737fa038f
Matheus Marinho <temarinho76@gmail.com>
2026-01-30 18:58:23 -0300

    ♻️ refactor: Remover código comentado e importações não utilizadas

--- src/app.py
+++ src/app.py
@@ -1,15 +1,8 @@
 from sqlalchemy import create_engine, text, Table, Column
 from sqlalchemy.schema import MetaData
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine import Engine
 from src.config.database import databases
 from src.core.extract.database_extractor import DatabaseExtractor
-
-Base = declarative_base()
-
-# class NfFaturemento(Base):
-#     __tablename__ = "nf_faturemento"
-    
 
 class App():
     def __init__(self, sources: list) -> None:
