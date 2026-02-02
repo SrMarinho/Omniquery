b47ed38294dd50f0d0c391098850edddc314ea21
Matheus Marinho <temarinho76@gmail.com>
2026-02-02 17:00:49 -0300

    ✨ feat: atualizar configuração do banco de dados para usar caminho absoluto e simplificar estrutura

--- src/utils/database_config_reader.py
+++ src/utils/database_config_reader.py
@@ -1,6 +1,12 @@
+from pathlib import Path
 import yaml
+from src.config.settings import base_path
 
 def get_database_config(database: str) -> None:
-    with open("databases.yaml", "r", encoding="utf-8") as file: 
+    database_file = "databases.yaml"
+    config_file = base_path / database_file
+    if not config_file.exists():
+        raise FileNotFoundError("File 'databases.yaml' not found in root path")
+    with open(config_file, "r", encoding="utf-8") as file: 
         data = yaml.safe_load(file)
         print(data)
